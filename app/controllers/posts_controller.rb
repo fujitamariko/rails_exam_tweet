6f303fbab6c6a88bc6a743505bab8fe1b705008d
fujitamariko <fujyta.mrk@gmail.com>
2021-08-11 23:50:22 +0900

    “コントローラにfindメソッドを設定”

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,7 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
     def edit
+        @post = Post.find(params[:id])
     end
     private
     def post_params
