19ecbf001fbf97cad77da2b2f485674ccd5622a4
fujitamariko <fujyta.mrk@gmail.com>
2021-08-12 00:23:27 +0900

    “コントローラにdestroyアクションを設定”

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,8 @@ class PostsController < ApplicationController
             render :edit
         end
     end
+    def destroy
+    end
     private
     def post_params
         params.require(:post).permit(:content)
